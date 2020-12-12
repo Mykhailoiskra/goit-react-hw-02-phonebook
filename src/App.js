c9930633e69dcf688ced2534985a62ea45713858
Mykhailo Iskra <iskramichel@gmail.com>
2020-12-12 23:58:52 +0200

    makes contacts list and delete feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { Component } from "react";
 import "./App.css";
+import ContactList from "./Components/ContactList/ContactList.jsx";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+
+        <h2>Contacts</h2>
+        <ContactList
+          contacts={contacts}
+          onDeleteContact={this.deleteContact}
+        ></ContactList>
+      </div>
+    );
+  }
 }
 
 export default App;
